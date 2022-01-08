2872227055e2b3031377d4b0ff5459c35c58d408
Cgordon-bee <81937022+Cgordon-bee@users.noreply.github.com>
2022-01-08 02:51:51 +0000

    set up server successfully

--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
-const express = require ('express'),
-const dotenv
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config({path: './config/config.env'})
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log (`server connected`))
+
 
 
 
