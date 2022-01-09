8bdf43934f3c57dda0b64245922783e24a8d9683
jacqie williamson <jacqieq@me.com>
2022-01-09 15:19:40 +0000

    import LoginForm component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import RegisterForm from "./components/Register";
+import LoginForm from "./components/Login";
 
 function App() {
   return (
     <div className="App">
+      {/* rendering components */}
       <RegisterForm />
+      <LoginForm />
     </div>
   );
 }
